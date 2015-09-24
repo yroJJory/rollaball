9f68e456d07e2932ca13bec5355e0b1944390607
Jory <me@jory.org>
2015-09-24 14:46:22 +0200

    Added wall impact sounds and colliders, script to trigger them

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTrigger : MonoBehaviour {
+	public string eventName;
+	public GameObject theParentGameObject;
+
+	private GameObject thePlayer;
+	void OnTriggerEnter() {
+		if (AudioManager.FabricLoaded) {
+			AudioManager.PlaySound(eventName, theParentGameObject);
+		}
+	}
+}
